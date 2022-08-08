d5ec0d5f30c8231bf8c611c8bc0aef2e01cbe8bd
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-08-08 16:59:43 +0530

    Nested if else

--- Class/Clang4to5/001ConditionalStatements.c
+++ Class/Clang4to5/001ConditionalStatements.c
@@ -103,3 +103,24 @@
 // # get year from user and check its leap year or not 
 // # get 2 values from user and print greater number 
 // # get 3 angles from user and check its triangle or not
+#include<stdio.h>
+int main()
+{
+    int a1,a2,a3;
+
+    printf("Enter 1st angle = ");
+    scanf("%d",&a1);
+    printf("Enter 2nd angle = ");
+    scanf("%d",&a2);
+    printf("Enter 3rd angle = ");
+    scanf("%d",&a3);
+
+    if(a1+a2+a3 == 180)
+    {
+        printf("This is triangle\n");
+    }
+    else
+    {
+        printf("This is not a triangle\n");
+    }
+}
