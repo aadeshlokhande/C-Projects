73b567fa10dcdaed6c5e4ac189615977dfb54959
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-08-09 17:07:42 +0530

    09-08-2022

---
+++ Class/forloop.c
@@ -0,0 +1,33 @@
+// loop
+// for 
+
+// for(inti; cond; inc/dec)
+// {
+//     statements
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     for(int a = 1; a<10; a++)
+//     {
+//         printf("%d\n",a);
+//     }
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     for(int a = 10; a>=1; a--)
+//     {
+//         printf("%d\n",a);
+//     }
+// }
+
+// +++++++++++++++++++++++++++++++++++++++++
+// Homework
+// print all even numbers from 0 to 100
+// get a number from user and print the table 
+// print reverse countdown from 50 to 0 
+
+
