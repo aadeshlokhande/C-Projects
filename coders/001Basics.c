7cb6b1c40dde49771dfff5c598294f1b4c5e316f
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-08-24 17:10:24 +0530

    24-08-2022

---
+++ coders/001Basics.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+int main()
+{
+    printf("\tHello world\n");
+    printf("H\tello world\n");
+    printf("He\tllo world\n");
+    printf("Hel\tlo world\n");
+    printf("Hell\to world\n");
+    printf("Hello\t world\n");
+    printf("Hello \tworld\n");
+    return 0;
+}
+// ===================
+
+// #include - preprocessing command 
+// stdio - library 
+// .h - extension
+// int - return type
+// main() - main Function
+// {} - scope 
+// printf() - function - print string 
+// "Hello world" - string 
+// ; - full stop - statement terminator
+// return - keyword (it return the value)
+// 0 - value
+
+// comments - notes 
+//single line comment -  // print kr raha hu hello world
+
+// multiline comment - /* ---------- */
+/* this 
+is 
+my
+multiline 
+comment
+*/
+
+// whitespace - blankspace
+
+// tokens - smallest elements
+// printf
+// (
+//     "Hello world"
+//     )
+//     ;
+
+// keywords - reserve words = 32
+// Keywords in C Programming
+// auto	break	case	char
+// const	continue	default	do
+// double	else	enum	extern
+// float	for	goto	if
+// int	long	register	return
+// short	signed	sizeof	static
+// struct	switch	typedef	union
+// unsigned	void	volatile	while
+
+// escape char 
+// \n :- new line 
+// \t :- horizontal tab
