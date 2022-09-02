7fba39625d52b9d594a9dee462e87aaccf37b56e
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-02 16:32:46 +0530

    02-09-2022

---
+++ Class/15multiDimensionalArray.c
@@ -0,0 +1,35 @@
+// int a[10] = {12,43,54,23,54,24,665,34,32,24};
+
+// datatype arrayName[size][size]; 
+// datatype arrayName[size][size] = {{values},{values},{values}....}; 
+
+// #include<stdio.h>
+// int main()
+// {
+//     int array[3][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+//     printf("%d\n",array[2][1]);
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int array[3][4] = {{10,20,30,40},{50,60,70,80},{90,10,11,12}};
+//     // int array[3][4];
+//     for(int i = 0; i<3; i++)
+//     {
+//         for(int j = 0; j<4; j++)
+//         {
+//             printf("Enter a number = ");
+//             scanf("%d",&array[i][j]);
+//         }   
+//     }
+    
+//     for(int i = 0; i<3; i++)
+//     {
+//         for(int j = 0; j<4; j++)
+//         {
+//             printf("%d\t",array[i][j]);
+//         }   
+//         printf("\n");
+//     }
+// }
