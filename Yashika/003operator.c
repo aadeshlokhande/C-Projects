8348f17a13eb370acc47ec920f993c117de2d017
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-19 18:29:58 +0530

    19-09-2022

---
+++ Yashika/003operator.c
@@ -0,0 +1,57 @@
+// operator
+
+// arithmatic
+// unary 
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 10,b;
+//     printf("%d\n",a);
+    // printf("%d\n",+a);
+    // printf("%d\n",-a);
+    // ++a;
+    // printf("%d\n",a);
+    // --a;
+    // printf("%d\n",a);
+
+    // pre
+    // b = ++a;
+    // printf("A = %d and B = %d\n",a,b);
+
+    // post 
+    // b = a++;
+    // printf("A = %d and B = %d\n",a,b);
+// }
+
+
+// binary 
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 10, b = 20;
+//     printf("%d\n",a + b);
+//     printf("%d\n",a - b);
+//     printf("%d\n",a * b);
+//     printf("%d\n",a / b);
+//     printf("%d\n",a % b);
+// }
+
+
+// relationship 
+// True = 1 
+// False = 0
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 10, b = 20;
+//     printf("%d\n",a > b);
+//     printf("%d\n",a >= b);
+//     printf("%d\n",a < b);
+//     printf("%d\n",a <= b);
+//     printf("%d\n",a == b);
+//     printf("%d\n",a != b);
+// }
+
+
+
