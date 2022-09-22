9179694f57ba330df49a7657fbd1af478f75afe6
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-22 16:34:25 +0530

    22-09-2022

--- Class/020fileHandling.c
+++ Class/020fileHandling.c
@@ -12,17 +12,36 @@
 // ---------------------
 
 // Read mode 
-#include<stdio.h>
-int main()
-{
-    FILE *ptr;
-    char data[20];
-    ptr = fopen("myfile.txt","r");
-    fscanf(ptr, "%s",data);
-    printf("my data is %s\n",data);
-    fclose(ptr);
-}
-orasore
+// #include<stdio.h>
+// int main()
+// {
+//     FILE *ptr;
+//     char data[20];
+//     ptr = fopen("myfile.txt","r");
+//     fscanf(ptr, "%s",data);
+//     printf("my data is %s\n",data);
+//     fclose(ptr);
+// }
+
 
 // Write mode 
+// #include<stdio.h>
+// int main()
+// {
+//     FILE *file;
+//     file = fopen("myfile001.txt","w");
+//     fprintf(file, "HEllo, \nbuddy how are you");
+//     fclose(file);
+// }
+
+
+
 // Append mode 
+// #include<stdio.h>
+// int main()
+// {
+//     FILE *file;
+//     file = fopen("myfile.txt","a");
+//     fprintf(file, "HEllo, buddy how are you\n");
+//     fclose(file);
+// }
