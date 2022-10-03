e173064d8b2b5bc3f15492fe3a6e7106022664c9
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-10-03 18:50:00 +0530

    03-10-2022

---
+++ coders/020Array.c
@@ -0,0 +1,55 @@
+// Array 
+// #include<stdio.h>
+// int main()
+// {
+    // datatype arrayName[size]
+    // int ary[6];
+    // ary[2] = 33;
+    // printf("Enter a number = ");
+    // scanf("%d",&ary[2]);
+    // printf("%d\n",ary[2]);
+
+    // for(int i=0; i<6; i++)
+    // {
+    //     printf("Enter a number = ");
+    //     scanf("%d",&ary[i]);
+    // }
+
+    // for(int i=0; i<6; i++)
+    // {
+    //     printf("array[%d] =%d\n",ary[i]);
+    // }
+
+
+    // int a[5] = {1,2,3,4,5};
+    // printf("%d\n",a[4]);
+
+//     for(int i=0; i<5; i++)
+//     {
+//         printf("array[%d] =%d\n",i,a[i]);
+//     }
+// }
+
+// -----------------------------------
+// #include<stdio.h>
+// int main()
+// {
+//     int size;
+//     printf("Enter a number = ");
+//     scanf("%d",&size);
+//     int a[size];
+    
+//     for(int b = 0; b<size; b++)
+//     {
+//         printf("Enter a number = ");
+//         scanf("%d",&a[b]);
+//     }
+// }
+
+
+// ----------------------------
+// Homework 
+// Get 10 numbers from user and print reverse
+// get 10 numbers from user and print total sum 
+// get 10 numbers and print all even numbers 
+// get 10 numbers and print number of odd numbers
---
+++ coders/tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+or(int i=0; i<6; i++)
+    {
+        printf("Enter a number = ");
+        scanf("%d",&ary[i]);
+    }
+
+    for(int i=0; i<6; i++)
+    {
+        printf("array[%d] =%d\n",ary[i]);
+    }
