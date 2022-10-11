7802cdd70f9e9881d47e4b4d28ff52cd05b90a05
Aadesh Lokhande <aadeshlokhande11@gmail.com>
2022-10-11 18:10:20 +0530

    11-10-2022

--- Yashika/010NestedForloop.c
+++ Yashika/010NestedForloop.c
@@ -73,3 +73,15 @@
 
 
 // 1 to 10 tables print 
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i = 1;i<=10;i++)
+//     {
+//         for(int j = 1; j<=10; j++)
+//         {
+//             printf("%d\t",i*j);
+//         }
+//         printf("\n");
+//     }
+// }
