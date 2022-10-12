0df12d96ad4abc733754a3ce389d71d6a8a31d65
Aadesh Lokhande <aadeshlokhande11@gmail.com>
2022-10-12 17:32:24 +0530

    12-10-2022

---
+++ Yashika/tempCodeRunnerFile.c
@@ -0,0 +1,19 @@
+include<stdio.h>
+// int main()
+// {
+//     for(int i = 1; i<=3; i++)
+//     {
+//         for(int j = 1; j<=3; j++)
+//         {
+//             if(i==1)
+//             {
+//                 printf("1 ");
+//             }
+//             else
+//             {
+//                 printf("0 ");
+//             }
+//         }
+//         printf("\n");
+//     }
+// }
