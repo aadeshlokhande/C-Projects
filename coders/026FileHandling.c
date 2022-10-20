334549c3d3ee2729bc85e5321944c934da3855a0
Aadesh Lokhande <aadeshlokhande11@gmail.com>
2022-10-20 18:11:30 +0530

    20-10-2022

---
+++ coders/026FileHandling.c
@@ -0,0 +1,16 @@
+// file handling 
+
+// RAM 
+// fast 
+// expensive - 2500 - 8GB
+// volatile
+
+// ROM
+// slow 
+// cheap - 2500 - 500GB
+// non-volatile
+
+
+// read mode 
+// write mode 
+// append mode 
