8e29d864a4a7e8ff9fcbd23aed7cec79f1d505ef
Aadesh Lokhande <aadeshlokhande11@gmail.com>
2022-10-21 18:01:51 +0530

    21-10-2022

--- coders/026FileHandling.c
+++ coders/026FileHandling.c
@@ -10,7 +10,67 @@
 // cheap - 2500 - 500GB
 // non-volatile
 
+// write mode 
+// #include<stdio.h>
+// int main()
+// {
+//     FILE *ptr;
+//     ptr = fopen("Coders.txt","w");
+//     fprintf(ptr, "good morning\n");
+//     fclose(ptr);
+// }
+
 
 // read mode 
-// write mode 
+// #include<stdio.h>
+// int main()
+// {
+//     char data[50];
+//     FILE *file;
+//     file = fopen("Coders.txt","r");
+//     fscanf(file, "%[^\n]s", data);
+//     fclose(file);
+//     printf("%s\n",data);
+// }
+
+
+
+
 // append mode 
+// #include<stdio.h>
+// int main()
+// {
+//     FILE *ptr;
+//     ptr = fopen("Coders.txt","a");
+//     fprintf(ptr, "hello kaveri\n");
+//     fclose(ptr);
+// }
+
+
+
+// ------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
